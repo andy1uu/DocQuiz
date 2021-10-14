1ec8c6bb7e3131982f81f58d67392f22ad9f3d76
Andy <andyluu324@gmail.com>
2021-10-13 22:54:46 -0400

    updated html, css, and js

--- index.js
+++ index.js
@@ -2,16 +2,45 @@
 *   Author: Andy Luu
 *   Date: 10.13.21
 *
-*   Description: 
+*   Description: This is the javascript for the important parts 
+*                of a document selector.
 *
 */
 
+currentSection = ["This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence."];
+
+let putCurrentSection = function(){
+
+    $("#current-section").empty();
+
+    for(let currentSectionSentenceIndex = 0; currentSectionSentenceIndex < currentSection.length; currentSectionSentenceIndex++){
+    
+        currentSentenceHTML = "<span class = \"selectable\">" + currentSection[currentSectionSentenceIndex] + "</span>";
+
+        $("#current-section").append(currentSentenceHTML + " ");
+    }
+}
+
+let increaseTextSize = function(){
+    
+}
+
+let decreaseTextSize = function(){
+    
+}
+
+let selectSentence = function(){
+    $(this).toggleClass("selected");
+}
+
 $(document).ready(function(){
 
-    $('span').hover(function(){
-        $(this).parent().children('span').css("background-color", "green");
-    }, function(){
-        $(this).parent().children('span').css("background-color", "none");
-    });
+    putCurrentSection();
+
+    $("#increase-text-size").click(increaseTextSize);
+
+    $("#decrease-text-size").click(decreaseTextSize);
+
+    $(".selectable").click(selectSentence);
 
 })
