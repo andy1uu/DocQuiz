4d5b2639b0924167e090915f4db47d4864e4a2bb
Andy Luu <andyluu324@gmail.com>
2021-10-21 12:07:54 -0400

    updated css and js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@
 
 currentSection = ["This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence.", "This is an example sentence."];
 
+currentCharacterCount = 0;
+
+sectionCharacterCount = 0;
+
+defaultTextSize = 16;
+
 let putCurrentSection = function(){
 
     $("#current-section").empty();
@@ -21,16 +27,38 @@ let putCurrentSection = function(){
     }
 }
 
+let loadNextSection = function(){
+
+}
+
+let submitButtonFunction = function(){
+
+}
+
 let increaseTextSize = function(){
-    
+    var text = document.getElementById('current-section');
+    var style = window.getComputedStyle(text, null).getPropertyValue('font-size');
+    var currentSize = parseFloat(style);
+    text.style.fontSize = (currentSize + 1) + 'px';
 } 
 
 let decreaseTextSize = function(){
-    
+    var text = document.getElementById('current-section');
+    var style = window.getComputedStyle(text, null).getPropertyValue('font-size');
+    var currentSize = parseFloat(style);
+    text.style.fontSize = (currentSize - 1) + 'px';
+
 }
 
 let selectSentence = function(){
+
     $(this).toggleClass("selected");
+    if($(this).hasClass("selected")){
+        $(this).css("background-color", "chartreuse");
+    }
+    else{
+        $(this).css("background-color", "");
+    }
 }
 
 $(document).ready(function(){
