72aab7eb43cb8ef82e8ef56429768481a7857d8e
Andy Luu <andyluu324@gmail.com>
2021-12-08 13:28:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 //should take in a paragraph, which is then split into different sentences using some split function, make sure that you maintain ending punctuation, make sure to account for line breaks, Mr. , 
 
-currentParagraph = "Note: Students entering the PhD program prior to Spring 2020 are eligible to follow the old rules shown here, but are almost certain to prefer the current rules shown on this page. A student pursuing the Ph.D. degree is expected to exhibit a comprehensive knowledge of a broad cross section of the computer science discipline and to contribute significant new knowledge to the discipline through the research contribution contained in the doctoral dissertation. A PhD student must complete a minimum of 90 credits of graduate study, of which at least 27 must derive from graded courses, with a minimum GPA of 3.0. The PhD program is intended to be completed in about five years from entering the graduate program with a BS degree in Computer Science or a related field, or about four years if the student already has an MS degree in Computer Science or a related field. This is possible because students who begin the PhD program already in possession of a Masters may be able to count as many as four courses toward their course requirement (see section Transfer Credits). To fulfill graduation requirements for the Ph.D. degree, students must satisfy the breadth requirement, adhere to an appropriate credit distribution, enroll in the graduate seminar, comply with the ethics requirement, and complete the major milestones for the degree, including the preliminary exam, research defense and final defense.";
+currentParagraph = "Note: Students entering the PhD program prior to Spring 2020 are eligible to follow the old rules shown here, but are almost certain to prefer the current rules shown on this page. A student pursuing the PhD degree is expected to exhibit a comprehensive knowledge of a broad cross section of the computer science discipline and to contribute significant new knowledge to the discipline through the research contribution contained in the doctoral dissertation. A PhD student must complete a minimum of 90 credits of graduate study, of which at least 27 must derive from graded courses, with a minimum GPA of 3. The PhD program is intended to be completed in about five years from entering the graduate program with a BS degree in Computer Science or a related field, or about four years if the student already has an MS degree in Computer Science or a related field. This is possible because students who begin the PhD program already in possession of a Masters may be able to count as many as four courses toward their course requirement (see section Transfer Credits). To fulfill graduation requirements for the PhD degree, students must satisfy the breadth requirement, adhere to an appropriate credit distribution, enroll in the graduate seminar, comply with the ethics requirement, and complete the major milestones for the degree, including the preliminary exam, research defense and final defense.";
 
 edgeCases = ["Mr.", "Mrs.", "Ms.", "Dr.", "St.", "Inc.", "Ltd.", "Jr.", "Sr.", "Co.", ".com", ".gov", ".net", ".org", ".io"];
 
@@ -23,7 +23,7 @@ currentCharacterCount = 0;
 
 sectionCharacterCount = currentParagraph.length;
 
-currentThreshold = currentCharacterCount/sectionCharacterCount;
+percentageOfCharactersSelected = currentCharacterCount/sectionCharacterCount;
 
 let putCurrentSection = function(){
 
@@ -35,10 +35,6 @@ let putCurrentSection = function(){
 
         $("#current-section").append(currentSentenceHTML + " ");
     }
-}
-
-let loadNextSection = function(){
-
 }
 
 let submitButtonFunction = function(){
@@ -76,25 +72,36 @@ let selectSentence = function(){
     if(currentThreshold <= threshold || $(this).hasClass("selected")){
         $(this).toggleClass("selected");
         if($(this).hasClass("selected")){
+
+            //Change this color, possibly yellow
+
             $(this).css("background-color", "chartreuse");
-            //grabs sentence, get the number of words from the setence, update the total words selected
+            //grabs sentence, get the number of characters from the setence, update the total characters selected
             currentCharacterCount += currentSection[parseInt($(this).attr('id'))].length;
+            
             currentThreshold = currentCharacterCount/sectionCharacterCount;
+
+            if(currentThreshold > threshold){
+                $("#submit").prop('disabled', false);
+            }
         }
         else{
             $(this).css("background-color", "");
             //opposite of selecting
             currentCharacterCount -= currentSection[parseInt($(this).attr('id'))].length;
             currentThreshold = currentCharacterCount/sectionCharacterCount;
-        }
 
-        $("#submit").prop('disabled', true);
+            if(currentThreshold <= threshold){
+                $("#submit").prop('disabled', true);
+            }
+        }
 
     }
     else{
+
+        //CHANGE THIS
         alert("You may not select anymore sentences!");
 
-        $("#submit").prop('disabled', false);
     }
 }
 
