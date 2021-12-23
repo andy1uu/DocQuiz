00446843c42f70e4cf32b110b3de9519e8868621
Andy Luu <andyluu324@gmail.com>
2021-12-23 17:12:28 -0500

    updated html, css, and js

--- index.js
+++ index.js
@@ -7,11 +7,8 @@
 *
 */
 
-//should take in a paragraph, which is then split into different sentences using some split function, make sure that you maintain ending punctuation, make sure to account for line breaks, Mr. , 
-
-/* currentParagraph = "Note: Students entering the PhD program prior to Spring 2020 are eligible to follow the old rules shown here, but are almost certain to prefer the current rules shown on this page. A student pursuing the PhD degree is expected to exhibit a comprehensive knowledge of a broad cross section of the computer science discipline and to contribute significant new knowledge to the discipline through the research contribution contained in the doctoral dissertation. A PhD student must complete a minimum of 90 credits of graduate study, of which at least 27 must derive from graded courses, with a minimum GPA of 3. The PhD program is intended to be completed in about five years from entering the graduate program with a BS degree in Computer Science or a related field, or about four years if the student already has an MS degree in Computer Science or a related field. This is possible because students who begin the PhD program already in possession of a Masters may be able to count as many as four courses toward their course requirement (see section Transfer Credits). To fulfill graduation requirements for the PhD degree, students must satisfy the breadth requirement, adhere to an appropriate credit distribution, enroll in the graduate seminar, comply with the ethics requirement, and complete the major milestones for the degree, including the preliminary exam, research defense and final defense."; */
-
 paragraphs = [["Note: Students entering the PhD program prior to Spring 2020 are eligible to follow the old rules shown here, but are almost certain to prefer the current rules shown on this page.", "A student pursuing the Ph.D. degree is expected to exhibit a comprehensive knowledge of a broad cross section of the computer science discipline and to contribute significant new knowledge to the discipline through the research contribution contained in the doctoral dissertation.", "A PhD student must complete a minimum of 90 credits of graduate study, of which at least 27 must derive from graded courses, with a minimum GPA of 3.0. The PhD program is intended to be completed in about five years from entering the graduate program with a BS degree in Computer Science or a related field, or about four years if the student already has an MS degree in Computer Science or a related field.", "This is possible because students who begin the PhD program already in possession of a Masters may be able to count as many as four courses toward their course requirement (see section Transfer Credits)."],["To fulfill graduation requirements for the Ph.D. degree, students must satisfy the breadth requirement, adhere to an appropriate credit distribution, enroll in the graduate seminar, comply with the ethics requirement, and complete the major milestones for the degree, including the preliminary exam, research defense and final defense.", "To encourage Ph.D. graduates to exhibit sufficient breadth of computer science areas, Ph.D. students must take CS courses at the 5000 and 6000 levels that span four (4) different areas.", "Transfer courses may be used to satisfy the breadth requirement. The available courses and areas are listed here."]]
+
 
 threshold = 0.2;
 
@@ -40,17 +37,32 @@ let putCurrentSection = function(sectionNum){
             currentSentenceHTML = "<span class = \"selectable\"" + "id = "+currentSectionSentenceIndex+">"+ paragraphs[sectionNum][currentSectionSentenceIndex] + "</span>";
 
             $("#current-section").append(currentSentenceHTML + " ");
+
+            sectionCharacterCount += paragraphs[sectionNum][currentSectionSentenceIndex].length;
         }
 
+        $("#submit").prop('disabled', true);
+
+        $(".selectable").click(function(){
+    
+            selectSentence($(this), currentSectionNum);
+    
+        });
+
     }
+    else{
+        
+        currentSentenceHTML = "<div class = \"no-more-sections\">There are no more sections to select.</div>";
 
-    //$("#submit").prop('disabled', true);
+        $("#current-section").append(currentSentenceHTML);
 
-    $(".selectable").click(function(){
+        $("#submit").prop('disabled', true);
 
-        selectSentence($(this), currentSectionNum);
+        $("#increase-text-size").prop('disabled', true);
 
-    });
+        $("#decrease-text-size").prop('disabled', true);
+
+    }
 }
 
 let submitButtonFunction = function(){
